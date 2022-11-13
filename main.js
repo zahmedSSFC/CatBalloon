1aca9adc9dd8bedea1ea5959ac52856338c2de08
zahmedSSFC <118121080+zahmedSSFC@users.noreply.github.com>
2022-11-13 15:40:07 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("canvas");
+var context  = canvas.getContext("2d");
+
+var img = new Image();   
+img.onload = function(){
+ 
+};
+
+// Set source path for image
+img.src = "images/cat.png";
+
+
+
